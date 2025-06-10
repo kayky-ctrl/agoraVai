5469694ccef62256afd9b73f45dc08f0c531ef6f
kayky-ctrl <kaykyrdepaula@gmail.com>
2025-06-10 17:24:00 -0300

    Commit final do dia 10/06/2025

--- script.js
+++ script.js
@@ -1,44 +1,48 @@
-const btnAdd = document.getElementById('btn-add');
-const tarefa = document.getElementById('tarefa');
-const taskList = document.getElementById('taskList');
-const titulo = document.getElementById('titulo');
+const btnadd =document.getElementById('btn-add');
+const tarefa = document.getElementById('tarefa')
+const tastlist = document.getElementById('tasklist')
+const titulo = document.getElementById('titulo')
 
-let nome = prompt('Qual o seu nome?');
-if (nome == "") {
-    titulo.innerHTML = "Lista de tarefas de generica";
-}   
-else{
-    titulo.innerHTML = `Lista de tarefas de ${nome}`;
+let nome = prompt("qual o seu nome? ")
+if(nome==""){
+    titulo.innerHTML = "lista de tarefa Genérica"
 }
+else{
+titulo.innerHTML =`Lista de tarefa: ${nome}`;
+}
+// acompanha o evento de clique do botão adicionar tarefa
+btnadd.addEventListener("click",criaTarefa);
 
-btnAdd.addEventListener('click', function() {
-    if (tarefa === '') {
-        alert('Por favor, insira uma tarefa.');
-        return;
-    }   
-
-    const li = document.createElement('li');
-    li.textContent = tarefa.value;
-    taskList.appendChild(li);
+function criaTarefa(){
+    const listItem =document.createElement('li');
+    listItem.textContent = tarefa.value;
+    tasklist.appendChild(listItem);
     tarefa.value = ''; 
 
+    //criar botão de deletar tarefa
     const removeButton = document.createElement('button');
-    removeButton.id = "remove"
-    removeButton.textContent = "x"
-    li.appendChild(removeButton);
+    removeButton.id ="remove";
+    removeButton.textContent ="X"
 
+    removeButton.addEventListener("click", function(){
+        tasklist.removeChild(listItem)
+    })
+
+
+    //criar botão de concluir tarefa
     const concluirButton = document.createElement('button');
-    concluirButton.id = "conclui";
-    concluirButton.textContent = "✓"
-    li.appendChild(concluirButton);
+    concluirButton.id="conclui";
+    concluirButton.textContent="✓"
+
+    // LISTEitem 
+    let buttonsItem = document.createElement('div');
+    buttonsItem.classList.toggle('buttonsItem')
+    listItem.appendChild(buttonsItem);
+    buttonsItem.appendChild(concluirButton);
+    buttonsItem.appendChild(removeButton);
+
     
-    removeButton.addEventListener('click', function(){
-        taskList.removeChild(li);
+    concluirButton.addEventListener("click",function(){
+        listItem.classList.toggle('completed');
     })
-
-    concluirButton.addEventListener('click', function(){
-        li.classList.toggle('completed');
-    })
-})
-
-
+}
